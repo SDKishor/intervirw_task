c4032042014fb0a8f24694444955c74b9f3c559a
SDKishor <91415355+SDKishor@users.noreply.github.com>
2023-04-28 04:49:05 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  const toggleButton = document.getElementsByClassName("toggle-button")[0];
+  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+  const navLinks = document.querySelectorAll(".navbar-links ul li a");
+
+  toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      navbarLinks.classList.toggle("active");
+    });
+  });
+};
