5e929df44463c99fa6264a54fc3e5fd810cd39ce
SDKishor <91415355+SDKishor@users.noreply.github.com>
2023-04-28 04:53:05 +0600

    a

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-window.onload = function () {
-  const toggleButton = document.getElementsByClassName("toggle-button")[0];
-  const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-  const navLinks = document.querySelectorAll(".navbar-links ul li a");
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const navLinks = document.querySelectorAll(".navbar-links ul li a");
 
-  toggleButton.addEventListener("click", () => {
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
     navbarLinks.classList.toggle("active");
   });
-
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      navbarLinks.classList.toggle("active");
-    });
-  });
-};
+});
